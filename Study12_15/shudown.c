0b1c5ebd13d10bfb65196c455d167e254dd65871
yang512117616 <yyp512117616@126.com>
2021-12-16 11:53:50 +0800

    学习

---
+++ Study12_15/shudown.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <Windows.h>
+#include <string.h>
+
+
+int main()
+{
+	char put_t[32] = { 0 };
+
+	printf("你的电脑将在两分钟内关机，如果输入“我是大笨猪”则取消关机！！！\n\n\n请输入：>");
+	system("shutdown -s -t 120");
+	while (1)
+	{
+		scanf("%s", put_t);
+
+		if (0 == strcmp(put_t, "我是大笨猪"))
+		{
+			goto hear;
+		}
+		printf("输入有误，请重新输入：>");
+	}
+	
+
+
+hear:
+	system("shutdown -a");
+	system("cls");
+	printf("\n\n拜拜，大笨猪！！！\n\n");
+	return 0;
+}
